61f9329c8d2d181820a3e01f02e41f235640b2a2
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-03-31 12:40:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ elif st.session_state.download == True:
             for para in doc.paragraphs:
                 fullText.append(para.text)
             return '\n'.join(fullText)
-        getText(file_stream)
+        st.write(getText(file_stream))
     
     with st.popover("**:red[Delete This Essay]**"):
         agree = st.checkbox(':red[I understand this essay cannot be restored after being deleted.]')
