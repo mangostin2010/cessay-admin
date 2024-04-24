d816aa15d89dfba288868122c1f897d62cfcb083
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-04-24 12:46:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ if st.session_state.download == False:
     for x in st.session_state.response_list:
         if st.button(x.replace('.docx', ''), use_container_width=1):
             st.session_state.target = x
-            st.session_state.download = True
+            st.link_button("Go to gallery", f"/check_essay?target={x}")
             st.rerun()
 
 elif st.session_state.download == True:
