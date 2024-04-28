68bb1dc503a989aacc36d32f64c08d2b297dca73
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-04-28 15:39:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,6 @@ cookies = EncryptedCookieManager(
     # This prefix will get added to all your cookie names.
     # This way you can run your app on Streamlit Cloud without cookie name clashes with other apps.
     prefix="justin",
-    # You should really setup a long COOKIES_PASSWORD secret if you're running on Streamlit Cloud.
-    password=os.environ.get("COOKIES_PASSWORD", "My secret password"),
 )
 if not cookies.ready():
     # Wait for the component to load and send us current cookies.
