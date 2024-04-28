6d6d06c5d90c8760435a9ab9030a60452cd28e4b
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-04-28 15:26:56 +0900

    Update check_essay.py

--- pages/check_essay.py
+++ pages/check_essay.py
@@ -23,13 +23,12 @@ st.title('Check Student\'s Essay')
 st.divider()
 
 if 'file_downloaded' not in st.session_state:
-    global file_stream, file
     file = db.get(st.session_state.target)
-    file_stream = io.BytesIO(file.read())
+    st.session_state.file_stream = io.BytesIO(file.read())
     st.session_state.file_downloaded = True
 
 st.download_button(label=f"**Download :blue[{st.session_state.target}]**",
-                data=file_stream,
+                data=st.session_state.file_stream,
                 file_name=st.session_state.target,
                 mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
     
