42e8624e2f67aa85a229fd30a9ecdbdf43780ff9
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-04-28 15:21:52 +0900

    Update check_essay.py

--- pages/check_essay.py
+++ pages/check_essay.py
@@ -37,41 +37,39 @@ if 'file_downloaded' not in st.session_state:
 
 preview = st.expander('Preview this essay')
 
-with preview:
-    # st.warning('⚠️ This process is still in developing.')
-    def getText(file_stream):
-        doc = docx.Document(file_stream)
-        fullText = []
-        for para in doc.paragraphs:
-            fullText.append(para.text)
-        return '\n'.join(fullText)
-
-    # Preview Expander
-    file_content = getText(file_stream)
-    file_name = st.session_state.target.replace('.docx','')
-    divided_file_name = file_name.split('_')
-
-    # Preview Datas
-    date = divided_file_name[0]
-    name = divided_file_name[1]
-    topic = divided_file_name[2]
-
-    f'''### **:gray[{topic}]**'''
+def do_preview():
+    with preview:
+        # st.warning('⚠️ This process is still in developing.')
+        def getText(file_stream):
+            doc = docx.Document(file_stream)
+            fullText = []
+            for para in doc.paragraphs:
+                fullText.append(para.text)
+            return '\n'.join(fullText)
     
-    # Write Content
-    st.write(file_content.replace(date, '', 1).replace(name, '', 1).replace(',','',1).replace(topic, '', 1))
-
-with st.popover("**:red[Delete This Essay]**"):
-    agree = st.checkbox(':red[I understand this essay cannot be restored after being deleted.]')
-    if agree == False: st.session_state.understand = True
-    elif agree == True: st.session_state.understand = False
+        # Preview Expander
+        file_content = getText(file_stream)
+        file_name = st.session_state.target.replace('.docx','')
+        divided_file_name = file_name.split('_')
+    
+        # Preview Datas
+        date = divided_file_name[0]
+        name = divided_file_name[1]
+        topic = divided_file_name[2]
+    
+        f'''### **:gray[{topic}]**'''
         
-    if st.button('**:red[Delete]**', disabled=st.session_state.understand):
-        db.delete(st.session_state.target)
-        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+        # Write Content
+        st.write(file_content.replace(date, '', 1).replace(name, '', 1).replace(',','',1).replace(topic, '', 1))
+    
+    with st.popover("**:red[Delete This Essay]**"):
+        agree = st.checkbox(':red[I understand this essay cannot be restored after being deleted.]')
+        if agree == False: st.session_state.understand = True
+        elif agree == True: st.session_state.understand = False
+            
+        if st.button('**:red[Delete]**', disabled=st.session_state.understand):
+            db.delete(st.session_state.target)
+            streamlit_js_eval(js_expressions="parent.window.location.reload()")
 
 if st.button('Go Back to Main Page'): 
     st.switch_page('main.py')
-
-for x in range(5):
-    streamlit_js_eval(js_expressions="history.back();")
