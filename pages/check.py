9c6350374673c47b1bc79516be6bb02dc676f297
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-04-29 22:08:50 +0900

    Update check.py

--- pages/check.py
+++ pages/check.py
@@ -11,44 +11,47 @@ with open('style.css', encoding='UTF-8') as f:
 if 'target' not in st.session_state:
     st.switch_page('main.py')
 
-st.title('Check Student\'s Essay')
-st.divider()
+def do_all():
+    st.title('Check Student\'s Essay')
+    st.divider()
 
-st.download_button(label=f"**Download :blue[{st.session_state.target}]**",
-                data=get_essay_from_name(st.session_state.target),
-                file_name=st.session_state.target,
-                mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    st.download_button(label=f"**Download :blue[{st.session_state.target}]**",
+                    data=get_essay_from_name(st.session_state.target),
+                    file_name=st.session_state.target,
+                    mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
 
 
 
-preview = st.expander('Preview this essay')
+    preview = st.expander('Preview this essay')
 
-delete_popover = st.popover("**:red[Delete This Essay]**")
-with delete_popover:
-    agree = st.checkbox(':red[I understand this essay cannot be restored after being deleted.]')
-        
-    delete_button = st.button('**:red[Delete]**', disabled=not agree)
-    if delete_button: remove_essay_from_name(st.session_state.target)
+    delete_popover = st.popover("**:red[Delete This Essay]**")
+    with delete_popover:
+        agree = st.checkbox(':red[I understand this essay cannot be restored after being deleted.]')
+            
+        delete_button = st.button('**:red[Delete]**', disabled=not agree)
+        if delete_button: remove_essay_from_name(st.session_state.target)
 
-if st.button('Go Back to Main Page'):
-    st.switch_page('main.py')
+    if st.button('Go Back to Main Page'):
+        st.switch_page('main.py')
 
-if 'preview_loaded' not in st.session_state:
+    if 'preview_loaded' not in st.session_state:
+        with preview:
+            with st.spinner('Wait for it...'):
+                st.session_state.preview_content = get_text_from_file(get_essay_from_name(st.session_state.target))
+
     with preview:
-        with st.spinner('Wait for it...'):
-            st.session_state.preview_content = get_text_from_file(get_essay_from_name(st.session_state.target))
+        file_content = st.session_state.preview_content
+        file_name = st.session_state.target.replace('.docx','')
+        divided_file_name = file_name.split('_')
 
-with preview:
-    file_content = st.session_state.preview_content
-    file_name = st.session_state.target.replace('.docx','')
-    divided_file_name = file_name.split('_')
+        date = divided_file_name[0]
+        name = divided_file_name[1]
+        topic = divided_file_name[2]
 
-    date = divided_file_name[0]
-    name = divided_file_name[1]
-    topic = divided_file_name[2]
+        f'''### **:gray[{topic}]**'''
+        
+        # Write Content
+        st.write(file_content.replace(date, '', 1).replace(name, '', 1).replace(',','',1).replace(topic, '', 1))
+        st.session_state.preview_loaded = True
 
-    f'''### **:gray[{topic}]**'''
-    
-    # Write Content
-    st.write(file_content.replace(date, '', 1).replace(name, '', 1).replace(',','',1).replace(topic, '', 1))
-    st.session_state.preview_loaded = True
+do_all()
