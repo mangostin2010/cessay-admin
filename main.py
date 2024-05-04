25378cc9c6b8354725eec4b507516f5c49c71b02
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-05-04 21:32:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -33,6 +33,6 @@ for x in st.session_state:
 URL_STRING = "https://streamlit.io/"
 
 st.markdown(
-    f'<a href="{URL_STRING}" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Action Text on Button</a>',
+    f'<a href="{URL_STRING}" style={button_style}>Action Text on Button</a>',
     unsafe_allow_html=True
 )
