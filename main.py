46afb8f712dc87ed6ab025360401dcf19df04fd8
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-05-04 22:16:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,21 +18,19 @@ essay_list = st.session_state.essay_list
 st.title('Check Student\'s Essay')
 st.divider()
 
-for x in essay_list:
-    # Define a button for each essay
-    essay_button = st.button(x.replace('.docx', ''), use_container_width=True)
+a_style = """color: rgb(49, 51, 63);display: block ruby; text-align: center; text-decoration: none; font-size: 16px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);"""
 
-    if essay_button:
-        st.session_state["target"] = x
-        st.switch_page('pages/check.py')
+for x in essay_list:
+    URL_STRING = f"https://check-cessay.streamlit.app/check?target={x}"
+    #URL_STRING = f"http://127.0.0.1:8501/check?target={x}"
+    x_placeholder = x.replace('.docx', '')
+    
+    # Define a button for each essay
+    st.markdown(
+        f'<a href="{URL_STRING}" style="{a_style}" target="_self">{x_placeholder}</a>',
+        unsafe_allow_html=True
+    )
 
 # Removing everything in st.session_state dictionary
 for x in st.session_state:
     del st.session_state[x]
-
-URL_STRING = "https://streamlit.io/"
-
-st.markdown(
-    f'<a href="{URL_STRING}" style={button_style}>Action Text on Button</a>',
-    unsafe_allow_html=True
-)
